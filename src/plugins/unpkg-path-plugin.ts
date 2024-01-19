f62c6ae94883adade38f086dd784a54f2bf0e1df
ashishPatel10 <ashishpatel@ashishs-air.home>
2024-01-19 14:55:05 -0500

    dynamic fetching of package with require package in that index file

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,10 +10,20 @@ export const unpkgPathPlugin = () => {
         if(args.path==='index.js'){
             return { path: args.path, namespace: 'a' };
         }
-        else if(args.path==='tiny-test-pkg')
-        {
-            return { path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js", namespace: 'a' };
+        // else if(args.path==='tiny-test-pkg')
+        // {
+        //     return { path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js", namespace: 'a' };
+        // }
+        if (args.path.includes('./') || args.path.includes('../')){
+          return {
+            namespace:'a',
+            path:new URL(args.path,args.importer+'/').href
+          }
         }
+        return {
+          namespace:'a',
+          path:`https://unpkg.com/${args.path}`
+        };
         
       });
  

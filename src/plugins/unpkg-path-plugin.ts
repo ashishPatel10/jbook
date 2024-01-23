78b5ebe443df9892ea1726c68d5d3a9d1bbd6131
ashishPatel10 <ashishpatel@ashishs-air.home>
2024-01-23 18:09:15 -0500

    add the dynamic input value to on load and code refactoring on resolve

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -5,34 +5,36 @@ import localForage from 'localforage';
 const fileCache = localForage.createInstance({
   name:'filecache'
 });
- 
-(async ()=>{
-  await fileCache.setItem('color','red');
 
-  const color = await fileCache.getItem('color')
-  console.log(color);
-  
-})()
-
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode:string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
+      
+    build.onResolve({filter:/(^index\.js$)/},()=>{
+      return {path:'index.js',namespace:'a'}
+    })
+    build.onResolve({filter:/^\.+\//},(args:any)=>{
+      return {
+        namespace:'a',
+        path:new URL(args.path,'https://unpkg.com'+args.resolveDir+'/').href
+      }
+    })
+    build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args);
-        if(args.path==='index.js'){
-            return { path: args.path, namespace: 'a' };
-        }
+        // if(args.path==='index.js'){
+        //     return { path: args.path, namespace: 'a' };
+        // }
         // else if(args.path==='tiny-test-pkg')
         // {
         //     return { path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js", namespace: 'a' };
         // }
-        if (args.path.includes('./') || args.path.includes('../')){
-          return {
-            namespace:'a',
-            path:new URL(args.path,'https://unpkg.com'+args.resolveDir+'/').href
-          }
-        }
+        // if (args.path.includes('./') || args.path.includes('../')){
+        //   return {
+        //     namespace:'a',
+        //     path:new URL(args.path,'https://unpkg.com'+args.resolveDir+'/').href
+        //   }
+        // }
         return {
           namespace:'a',
           path:`https://unpkg.com/${args.path}`
@@ -46,20 +48,30 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React from 'react';
-              console.log(React);
-            `,
+            contents: inputCode,
           };
         } 
+
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+        if (cachedResult){
+          return cachedResult;
+        }
+
+
+
+
         const {data, request} = await axios.get(args.path)
         console.log(request);
         
-        return {
+        const result:esbuild.OnLoadResult = {
             loader: 'jsx',
             contents:data,
             resolveDir: new URL('./', request.responseURL).pathname
           };
+
+          await fileCache.setItem(args.path, result)
+          return result
         
       });
     },

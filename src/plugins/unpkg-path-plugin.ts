3b466388b922c1560eaaf89079ac59f89b2e6e02
ashishPatel10 <ashishpatel@ashishs-air.home>
2024-01-23 22:16:11 -0500

    added remote fetching of esbuild.wasm file instead of through locally in public folder

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,79 +1,26 @@
-import * as esbuild from 'esbuild-wasm';
-// import axios from 'axios'
-// import localForage from 'localforage';
-
-// const fileCache = localForage.createInstance({
-//   name:'filecache'
-// });
+import * as esbuild from "esbuild-wasm";
 
 export const unpkgPathPlugin = () => {
   return {
-    name: 'unpkg-path-plugin',
+    name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
-      
-    build.onResolve({filter:/(^index\.js$)/},()=>{
-      return {path:'index.js',namespace:'a'}
-    })
-    build.onResolve({filter:/^\.+\//},(args:any)=>{
-      return {
-        namespace:'a',
-        path:new URL(args.path,'https://unpkg.com'+args.resolveDir+'/').href
-      }
-    })
-    build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        // if(args.path==='index.js'){
-        //     return { path: args.path, namespace: 'a' };
-        // }
-        // else if(args.path==='tiny-test-pkg')
-        // {
-        //     return { path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js", namespace: 'a' };
-        // }
-        // if (args.path.includes('./') || args.path.includes('../')){
-        //   return {
-        //     namespace:'a',
-        //     path:new URL(args.path,'https://unpkg.com'+args.resolveDir+'/').href
-        //   }
-        // }
-        return {
-          namespace:'a',
-          path:`https://unpkg.com/${args.path}`
-        };
-        
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: "index.js", namespace: "a" };
       });
- 
-      // build.onLoad({ filter: /.*/ }, async (args: any) => {
-      //   console.log('onLoad', args);
- 
-      //   if (args.path === 'index.js') {
-      //     return {
-      //       loader: 'jsx',
-      //       contents: inputCode,
-      //     };
-      //   } 
-
-      //   const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-
-      //   if (cachedResult){
-      //     return cachedResult;
-      //   }
-
-
-
-
-      //   const {data, request} = await axios.get(args.path)
-      //   console.log(request);
-        
-      //   const result:esbuild.OnLoadResult = {
-      //       loader: 'jsx',
-      //       contents:data,
-      //       resolveDir: new URL('./', request.responseURL).pathname
-      //     };
-
-      //     await fileCache.setItem(args.path, result)
-      //     return result
-        
-      // });
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: "a",
+          path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/")
+            .href,
+        };
+      });
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log("onResolve", args);
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
     },
   };
 };
